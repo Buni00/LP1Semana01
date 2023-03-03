930c15f132940b781c2ed2b922ad21d3d7577a47
Bruno Fernandes <a22103346@alunos.ulht.pt>
2023-03-03 14:02:44 +0000

    Add print variables

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i= 1;
+            double u = 2.0;
+            float x = 3.0f;
+            long l = 4L;
+
+            Console.WriteLine(i + " "  + u +" " + x + " " + l);
+        }
+
+    }
+}
