72dd8441bb1837f2b4096253b0f53f505db31601
Bruno Fernandes <a22103346@alunos.ulht.pt>
2023-03-03 14:44:06 +0000

    Add other types os strings

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -11,7 +11,16 @@ namespace VariasStrings
             string c = @"Uma Plica 0x0027 em Unicode";
             string d = @"Literalmente um \n";
 
+            string x = "String "+ "conc" + "atizada";
+            string xy = x + "e com texto adicionado!";
+
+            int docesCompra = 3;
+            int docesComi =2;
+            string y = $"comprei {docesCompra} doces e comi {docesComi}";
+
             Console.WriteLine(a +"\n"+ b +"\n"+ c + "\n" + d);
+
+            Console.WriteLine(x +"\n"+ xy +"\n"+ y + "\n" + z + "\n" + last);
         }
     }
 }
