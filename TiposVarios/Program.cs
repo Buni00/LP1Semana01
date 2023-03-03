bb6af25d20bc66d8ca36a6b7003376ac4dfbab60
Bruno Fernandes <a22103346@alunos.ulht.pt>
2023-03-03 14:22:57 +0000

    Add print real numbers

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -7,16 +7,21 @@ namespace TiposVarios
         static void Main(string[] args)
         {
             int i= 1;
-            double u = 2.0;
-            float x = 3.0f;
-            long l = 4L;
+            uint u = 2U;
+            long l = 3L;
 
-            Console.WriteLine(i + " "  + u +" " + x + " " + l);
+            Console.WriteLine(i + " " + u + " " + l);
 
             char dot = '\u002E';
             char comma = '\u002C';
 
             Console.WriteLine(dot + " " + comma);
+
+            double real1 = 3.1415;
+            float real2 = 3.1415f;
+            decimal real3 = 3.1415m;
+
+            Console.WriteLine(real1 + " " + real2 + " " + real3);
         }
 
     }
